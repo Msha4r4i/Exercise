b52aace1bade866fd1727a68018c581b39f6c759
Msha4r4i <mshari.9420@gmail.com>
2025-06-25 21:15:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,167 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        for(int i =1; i <= 100;i++){
+//            if(i % 3==0 && i %5==0){
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5==0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3==0) {
+//                System.out.println("Fizz");
+//
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+
+//        String s = new Scanner(System.in).nextLine();
+//        String r = "";
+//        for (int i = s.length() - 1; i >= 0; i--)
+//            r += s.charAt(i);
+//        System.out.println(" reversed string :" + r);
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number :");
+//        int num = scanner.nextInt();
+//
+//        int result = 1;
+//
+//        for (int i = 1; i <= num; i++) {
+//            result *= i;
+//        }
+//        System.out.println("factorial :  " + num + "  is: " + result);
+//    }
+//}
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String choice;
+//
+//
+//        do {
+//            System.out.println("Enter two numbers to multiply");
+//            System.out.println("Enter the number 1 : ");
+//            int num1 = scanner.nextInt();
+//
+//
+//            System.out.println("Enter the number 2 :");
+//            int num2 = scanner.nextInt();
+//
+//            int process = num1 * num2;
+//            System.out.println("Result :" + process);
+//
+//
+//            scanner.nextLine();
+//            System.out.print("Do you want to try again? (yes/no): ");
+//            choice = scanner.nextLine();
+//
+//
+//        }while (choice.equals("yes"));{
+//            System.out.println("ended");
+//        }
+//
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the base : ");
+//        int base = scanner.nextInt();
+//
+//        System.out.println("Enter the exponent :");
+//        int exponent = scanner.nextInt();
+//
+//        int result = 1;
+//
+//        for (int i = 1; i <= exponent; i++) {
+//            result = result * base;
+//        }
+//        System.out.println( base + "+"+ exponent + "=" + result);
+//    }
+//}
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int num1 = 0;
+//        int num2 = 0;
+//        int number;
+//
+//        System.out.println("Enter number");
+//        while (true) {
+//            number = scanner.nextInt();
+//            {
+//                if (number == 0) {
+//                    break;
+//                }
+//                if (number % 2 == 0) {
+//                    num1 += number;
+//                } else {
+//                    num2 += number;
+//
+//                }
+//            }
+//            System.out.println(" even numbers : " + num1);
+//            System.out.println(" odd numbers : " + num2);
+//        }
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number :");
+//        int num = scanner.nextInt();
+//
+//        if (num <= 1) {
+//            System.out.println(" not prime");
+//        } else {
+//            for (int i = 2; i < num; i++) {
+//                if (num % i == 0) {
+//                    System.out.println("not prime");
+//                    return;
+//                }
+//            }
+//            System.out.println("prime");
+//        }
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//
+//        for (int week = 1; week <= 4; week++) {
+//            System.out.println("week" + week);
+//
+//
+//            for (int day = 1; day <= 7; day++) {
+//                System.out.println("Day" + day);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" Enter a word ");
+//        String word = scanner.nextLine();
+//        String r = "";
+//
+//        for (int i = word.length() - 1; i >= 0; i--) {
+//            r += word.charAt(i);
+//        }
+//        if (word.equals(r)) {
+//            System.out.println(" it's a palindrome ");
+//        } else {
+//            System.out.println(" not a palindrome");
+//        }
+//    }
+//}
+//
+
+
+
+
